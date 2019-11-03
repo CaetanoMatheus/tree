77bacb50330e9005a0b016292ab276ed6f64590e
Matheus Caetano <caetanomatheus876@gmail.com>
2019-11-03 12:39:53 -0200

    Binary tree is almost done.

--- binaryTree/BinaryTree.java
+++ binaryTree/BinaryTree.java
@@ -8,8 +8,8 @@ public class BinaryTree {
 
     public void insert(int value) { insert(this.root, value); }
 
-    public void insert(Node node, int value) {
-        if (node != null) {
+    private void insert(Node node, int value) {
+        if (this.root != null) {
             if (value < node.getKey()) {
                 if (node.getLeft() != null) insert(node.getLeft(), value);
                 else node.setLeft(new Node(value));
@@ -17,20 +17,63 @@ public class BinaryTree {
                 if (node.getRight() != null) insert(node.getRight(), value);
                 else node.setRight(new Node(value));
             }
-        } else node = new Node(value);
+        } else this.root = new Node(value);
+    }
+
+    public Node remove(int value) { return remove(this.root, value); }
+
+    public Node remove(Node root, int value) {
+        //TODO
+        return null;
+    }
+
+    public Node search(int value) { return search(this.root, value); }
+
+    public Node search(Node root, int value) {
+        if (root == null) return null;
+        if (root.getKey() == value) return root;
+        if (root.getKey() > value) return search(root.getLeft(), value);
+        return search(root.getRight(), value);
+    }
+
+    public void preOrder() { preOrder(this.root); }
+
+    public void preOrder(Node root) {
+        if (root != null) {
+            System.out.print(root.getKey() + ", ");
+            preOrder(root.getLeft());
+            preOrder(root.getRight());
+        }
+    }
+
+    public void inOrder() { inOrder(this.root); }
+
+    public void inOrder(Node root) {
+        if (root != null) {
+            inOrder(root.getLeft());
+            System.out.print(root.getKey() + ", ");
+            inOrder(root.getRight());
+        }
+    }
+
+    public void afterOrder() { afterOrder(this.root); }
+
+    public void afterOrder(Node root) {
+        if (root != null) {
+            afterOrder(root.getLeft());
+            afterOrder(root.getRight());
+            System.out.print(root.getKey() + ", ");
+        }
+    }
+
+    public int size() { return size(this.root); }
+
+    public int size(Node root) {
+        if (root == null) return 0;
+        return size(root.getLeft()) + size(root.getRight()) + 1;
     }
 
     public Node getRoot() { return this.root; }
 
     public void setRoot(Node root) { this.root = root; }
 }
-
-    
-        
-     
-
-    
-        
-     
-         
-    
--- binaryTree/Main.java
+++ binaryTree/Main.java
@@ -2,10 +2,18 @@ public class Main {
 
     public static void main(String[] args) {
         
-        BinaryTree b = new BinaryTree();
+        BinaryTree b = new BinaryTree(new Node(5));
+        Node n = new Node();
         b.insert(5);
         b.insert(4);
+        b.insert(1);
         b.insert(8);
-        
+        b.insert(0);
+        b.insert(8);
+        b.insert(3);
+        b.insert(7);
+        b.insert(2);
+        int a = b.size();
+        System.out.println(a);
     }
 }
